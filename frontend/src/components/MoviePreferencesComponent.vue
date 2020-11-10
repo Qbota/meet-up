4106068901350ae52fe0f1e04a1655c4c80fc928
Qbot <kubacembrzynski97@o2.pl>
2020-11-10 18:44:56 +0100

    set up

--- frontend/src/components/MoviePreferencesComponent.vue
+++ frontend/src/components/MoviePreferencesComponent.vue
@@ -1,11 +1,25 @@
 <template>
   <v-card>
-    Movie preferences
+    Add here movie preferences and action to add recently watched movies with rating
+    <v-btn @click="example()">Check browser console</v-btn>
   </v-card>
 </template>
 
 <script>
 export default {
-name: "MoviePreferencesComponent"
+name: "MoviePreferencesComponent",
+  data: function (){
+    return {
+      //Add here properties like this
+      someProp: 123,
+      anotherProp: null
+    }
+  },
+  methods: {
+    //add here methods
+    example(){
+      console.log('hi')
+    }
+  }
 }
 </script>
