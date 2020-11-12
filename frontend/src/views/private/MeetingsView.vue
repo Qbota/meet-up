ae3ac56ac2359e0b033a1e771a0dc593c30a5352
Qbot <kubacembrzynski97@o2.pl>
2020-11-12 17:51:30 +0100

    calendar set up

--- frontend/src/views/private/MeetingsView.vue
+++ frontend/src/views/private/MeetingsView.vue
@@ -1,11 +1,48 @@
 <template>
-  <v-content>
-    Meetings
-  </v-content>
+  <v-main>
+    <v-container>
+      <v-row>
+        <v-btn
+            icon
+            class="ma-2"
+            @click="$refs.calendar.prev()"
+        >
+          <v-icon>mdi-chevron-left</v-icon>
+        </v-btn>
+        <v-btn @click="setToday()">Today</v-btn>
+        <v-btn
+            icon
+            class="ma-2"
+            @click="$refs.calendar.next()"
+        >
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-row>
+
+
+      <v-sheet height="600">
+      <v-calendar ref="calendar" v-model="focus" type="month" :events="meetings">
+
+      </v-calendar>
+      </v-sheet>
+    </v-container>
+  </v-main>
 </template>
 
 <script>
 export default {
-name: "MeetingsView"
+name: "MeetingsView",
+  data: () => ({
+    focus: '',
+    meetings: [{
+      name: 'Test Meeting',
+      start: new Date()
+    }]
+  }),
+  methods: {
+    setToday () {
+      this.focus = ''
+    }
+  }
 }
 </script>
